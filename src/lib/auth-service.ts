efad2df2acf368c4dfa0c8244065908f7fe7c1a7
gur22-09 <gurprit.iiest@gmail.com>
2024-01-22 18:50:06 +0530

    fix: auth service error handling

--- src/lib/auth-service.ts
+++ src/lib/auth-service.ts
@@ -1,22 +1,23 @@
 import { currentUser } from '@clerk/nextjs';
 import { db } from './db';
-import { either, identity, ifElse, isNil, pipe, prop } from 'ramda';
+import { either, identity, ifElse, isNil, pipe, prop, defaultTo, partial } from 'ramda';
 import { throwError } from './utils';
 import { type User as ClerkUser } from '@clerk/nextjs/server';
 import { type User } from '@prisma/client';
 
 export async function getCurrentUser(): Promise<User> {
   const c = await currentUser();
-
   return pipe(
     ifElse(
-      either(isNil, pipe(prop('username'), isNil)),
-      throwError('unauthorized'),
+      either(isNil, pipe(prop('username'), defaultTo(''), isNil)),
+      () => throwError('unauthorized'),
       identity,
     ),
     (user: ClerkUser) => {
       return db.user.findUnique({ where: { externalUserId: user.id } });
     },
-    ifElse(isNil, throwError('no user found'), identity),
+    ifElse(isNil, () => throwError('no user found'), identity),
   )(c);
 }
+
+
--- src/lib/recommended-service.ts
+++ src/lib/recommended-service.ts
@@ -1,10 +1,48 @@
+import { andThen, pipe, tryCatch } from 'ramda';
 import { getCurrentUser } from './auth-service';
 import { db } from './db';
+import { User } from '@prisma/client';
 
 export async function getRecommended() {
-  return await db.user.findMany({
-    orderBy: {
-      createdAt: 'desc',
+  return pipe(
+    async () => {
+      try {
+        const user = await getCurrentUser();
+        return user.id;
+      } catch {
+        return '';
+      }
     },
-  });
+    // tryCatch(  TODO - find better tryCatch ramda alternative, currently it does not catch
+    // github issue - https://github.com/ramda/ramda/issues/2877
+    //   async () => {
+    //     const user = await getCurrentUser();
+    //     return user.id;
+    //   },
+    //   async () => {
+    //     return new Promise<string>((res) => res('')); // for effective composition return type should match
+    //   },
+    // ),
+    andThen(async (userId): Promise<User[]> => {
+      if (userId) {
+        return await db.user.findMany({
+          where: {
+            NOT: {
+              id: userId,
+            },
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        });
+      } else {
+        return await db.user.findMany({
+          orderBy: {
+            createdAt: 'desc',
+          },
+        });
+      }
+    }),
+    andThen((users) => users || []),
+  )();
 }
